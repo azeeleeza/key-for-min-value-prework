a47ef22a412c7919402f4de60738e7e540d8d37f
Leeza Luncheon <leezaluncheon@gmail.com>
2018-03-12 23:40:24 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+<<<<<<< HEAD
   min=nil
+=======
+  min=0
+>>>>>>> e83f952efcae4fd21044dab76911704a0d8619da
   minkey=""
   if name_hash == {}
     nil
   else
     name_hash.each do|key,value|
+<<<<<<< HEAD
       if nil == min || value < min
         min = value
         minkey=key
@@ -18,4 +23,16 @@ def key_for_min_value(name_hash)
   end
   minkey
 end
+=======
+      min = value
+      value > min ? min = min : min = value
+
+        minkey = key
+
+    end
+    puts min
+    puts minkey
+    minkey
+  end
+>>>>>>> e83f952efcae4fd21044dab76911704a0d8619da
 end
